84fc0d96212840bacddc1beb2461f2672b3bed5d
Michael Wender <michael@michaelwender.com>
2017-06-15 10:06:13 -0400

    Adding `dropbox_uploader` and crontab instructions

--- dropboxbackup.sh
+++ dropboxbackup.sh
@@ -10,6 +10,21 @@
 # - SITESTORE=/path/to/WordPress/installations
 # - DAYSKEEP=3
 #
+# This script requires dropbox_uploader to be installed. If you
+# need to add it to your setup, do the following:
+#
+#   1. sudo curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /usr/bin/dropbox_uploader
+#   2. sudo chmod +x /usr/bin/dropbox_uploader
+#
+# Now, you'll need to add your access token from an app you've
+# setup in your Dropbox account. When you run `dropbox_uploader`
+# it will walk you through the process.
+#
+# Finally, you'll need to setup a cron to call this script
+# daily. Example:
+#
+# 0 30 * * * bash ~/dropboxbackup.sh >/dev/null 2>&1 # Runs every day at 12:30am
+#
 # Credit: https://guides.wp-bullet.com/automatically-back-wordpress-dropbox-wp-cli-bash-script/
 
 # Check for `dropboxbackup-config.sh`
